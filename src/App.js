5917da131f27e06de63026c39c4da93a08a55090
aayahda <aadhyakraj@gmail.com>
2024-05-15 23:24:37 +0530

    bio added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Navbar from './components/navbar';
 import Headroom from 'react-headroom';
+import Bio from './components/Bio';
 
 function App() {
   return (
     <div className="App">
       <Headroom><Navbar/></Headroom>
+      <Bio/>
      </div>
   );
 }
---
+++ src/components/Bio.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Bio(){
+    return (
+        <div className="bio">
+            <ul >
+               <li><div className="phrase">
+               <div className="AFW"> <div className="circle"></div><p>   Available for Work</p></div>
+                <h1>I'm Aadhya K Raj<span>.</span> I Code <span>*</span> I Create <span>*</span> I Innovate<span>.</span></h1>
+                <p>Transforming Creative Ideas into Captivating Interactive Reality. </p>
+                </div></li>
+               <li><div><h1>image</h1></div></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Bio;
