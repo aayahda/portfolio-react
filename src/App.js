204e384729fa1258802da6affbd15f5549805dc4
aayahda <aadhyakraj@gmail.com>
2024-05-15 17:04:26 +0530

    headroom added to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import Headroom from 'react-headroom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-    </div>
+      <Headroom><Navbar/></Headroom>
+     </div>
   );
 }
 
