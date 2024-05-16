8bcc9b208db8d42559b816e53191ce5aee95c383
aayahda <aadhyakraj@gmail.com>
2024-05-16 23:26:08 +0530

    bento design added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,8 +6,12 @@ function About(){
         <div className="about">
             <h1>About Me</h1>      
         <div className="container">
-            <div >Experience</div>
-            <div >About</div>
+            <div className="exper" >Experience</div>
+            <div className="LinkedIn"> LinkedIn</div>
+            <div className="card" >About</div>
+            <div className="stack">Stack</div>
+            <div className="Project" >Projects</div>
+            
         </div>
         </div>
 
