346ebf5eb4ea321598bcd302135d9c864945ef3d
aayahda <aadhyakraj@gmail.com>
2024-05-16 15:42:19 +0530

    animation added

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Avatar from "./avatar";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 function Bio(){
+    useEffect(()=>{
+        AOS.init({duration:2000});
+    },[]);
     return (
         <div  className="bio">
             <ul >
-               <li><div className="phrase">
+               <li>
+               <div data-aos="fade-up" data-aos-easing="ease" >
+                <div className="phrase">
                <div className="AFW"> <div className="circle"></div><p>   Available for Work</p></div>
                 <h1>I'm Aadhya K Raj<span className="span">.</span> I Code <span className="span">*</span> I Create <span className="span">*</span> I Innovate<span className="span">.</span></h1>
                 <p className="Tag">Transforming Creative Ideas into Captivating Interactive Reality. </p>
-                </div></li>
+                </div>
+                </div>
+                </li>
                <li><Avatar/></li>
             </ul>
         </div>
--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-
+import React ,{ useEffect }from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Avatar(){
+    useEffect(()=>{
+        AOS.init({duration:2000});
+    },[]);
     return(
+        <div data-aos="fade-left" data-aos-easing="ease" >
         <div className="avatar">
           {/* <a href="https://imgbb.com/">
             <img style={{ width: '300px', height: '300px' }}src="https://i.ibb.co/qNkX9PV/Avatar.png" alt="Avatar" border="0"/></a> */}
             <a href="https://ibb.co/pn19Hkr"><img style={{ width: '300px', height: '300px' }} src="https://i.ibb.co/WsHYrRp/Avatar-Upscaled.png" alt="Avatar-Upscaled" border="0"/></a>
-        </div>
+        </div></div>
     );
 }
 
