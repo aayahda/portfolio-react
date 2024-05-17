d4a5c61fdcd9384d65493ed6f41144ae5fa388b2
aayahda <aadhyakraj@gmail.com>
2024-05-17 15:19:50 +0530

    bento: Experience added

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -5,26 +5,26 @@ function Experience(){
     return(
         <div className="expcontainer">
             <div  className="container3" >
-                <p>2023-now</p>
-                <ul>
-                <li><h3>Oracle</h3></li>
-                <li><p>Associate Consultant </p></li>
-                </ul>
+                <div><p style={{color:"white", fontSize:"18px"}}>2023-Now</p></div>
+                <div>
+                    <p style={{color:"white", fontSize:"18px"}}>Oracle</p>
+                    <p>Associate Consultant</p>
+                </div>
             </div>
-            <div className="container3">
-            <p>2023-now</p>
-                <ul>
-                <li><h3>Oracle</h3></li>
-                <li><p>Associate Consultant </p></li>
-                </ul>
+            <div  className="container3" >
+                <div><p style={{color:"white", fontSize:"18px"}}> 2022-2023</p></div>
+                <div>
+                    <p style={{color:"white", fontSize:"18px"}}>TGH</p>
+                    <p>Flutter Developer Intern</p>
+                </div>
             </div>
-            <div className="container3">
-            <p>2023-now</p>
-                <ul>
-                <li><h3>Oracle</h3></li>
-                <li><p>Associate Consultant </p></li>
-                </ul>
-            </div>      
+            <div  className="container3" >
+                <div><p style={{color:"white", fontSize:"18px"}}>2022-2023</p></div>
+                <div>
+                    <p style={{color:"white", fontSize:"18px"}}>IEDC</p>
+                    <p>WIE Lead</p>
+                </div>
+            </div>    
         </div>
     )
 }
