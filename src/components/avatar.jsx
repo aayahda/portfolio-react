cd6fba6f5a43eb5bd677e7f94a87a83ab5d238c2
aayahda <aadhyakraj@gmail.com>
2024-05-19 13:56:42 +0530

    avatar updated

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -9,7 +9,7 @@ function Avatar(){
     return(
         <div data-aos="fade-left" data-aos-easing="ease" >
         <div className="avatar">
-            <img src="../../Assets/pic.png"/>
+            <img src="../../Assets/Avatar.png" style={{ width: '310px', height: '310px' }}/>
            {/* <a href="https://ibb.co/pn19Hkr"><img style={{ width: '300px', height: '300px' }} src="https://i.ibb.co/WsHYrRp/Avatar-Upscaled.png" alt="Avatar-Upscaled" border="0"/></a> */}
         </div></div>
     );
