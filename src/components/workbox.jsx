31a7b79532de2cd515c82b8bae151222bac0098a
aayahda <aadhyakraj@gmail.com>
2024-05-27 16:48:21 +0530

    hover added

--- src/components/workbox.jsx
+++ src/components/workbox.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import '../works.css';
 
 function WorkBox(props){
+    const [isHovered, setIsHovered] = useState(false);
+    const backgroundImage = isHovered
+        ? `linear-gradient(to bottom, rgba(0,0,0,0) 0%, #151515 100%), url(${props.image})`
+        : `linear-gradient(to bottom, rgba(0,0,0,0) 50%, #151515 100%),url(${props.image})`;
+
     return( 
 <div className="work-box">
-<div className="gradient-bg"style={{ backgroundImage: `
-                    linear-gradient(to bottom, rgba(0,0,0,0) 50%,#151515 100%),
-                    url(${props.image})
-                `,
-               }}>
-    <div ><ul>
+<div className="gradient-bg"style={{ backgroundImage: backgroundImage, transition: 'background-image 0.3s ease-in-out'}}
+ onMouseEnter={() => setIsHovered(true)}
+ onMouseLeave={() => setIsHovered(false)}>
+    <div style={{display:isHovered?"none":"flex"}}><ul>
     {props.tech.map((tec, index) => (
                             <li key={index}>{tec}</li>
                         ))}
         </ul></div>
- <div className="desc">
+ <div className="desc" style={{display:isHovered?"none":"flex"}}>
     <h3>{props.heading}</h3>
     <p> {props.desc} </p>
  </div>
