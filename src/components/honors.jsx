b7ed1602bc43c20aa1a49974911109cbb257eff2
aayahda <aadhyakraj@gmail.com>
2024-05-29 23:17:12 +0530

    key accomplishment added

--- src/components/honors.jsx
+++ src/components/honors.jsx
@@ -12,16 +12,16 @@ function Honors(){
                     <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
             </div>
             <div className="box">
-                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/89/bb/ca/89bbcab73b8d5d37272c2ed534af1237.jpg)",backgroundSize:"contain"}}></div>
-                    <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
+                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/0b/f2/7e/0bf27e40a1101b2f659d50e95a0d5156.jpg)",backgroundSize:"contain"}}></div>
+                    <p> Fortunate to be the youngest Entrepreneur speaker at the at the Women In Business Conclave, Thrissur.</p>
             </div>
             <div className="box">
                 <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/89/bb/ca/89bbcab73b8d5d37272c2ed534af1237.jpg)",backgroundSize:"contain"}}></div>
-                    <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
+                    <p> Among the Top 12 teams in  National Level WiT Ace Hackathon in association with IBM.</p>
             </div>
             <div className="box">
-                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/89/bb/ca/89bbcab73b8d5d37272c2ed534af1237.jpg)",backgroundSize:"contain"}}></div>
-                    <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
+                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/0b/f2/7e/0bf27e40a1101b2f659d50e95a0d5156.jpg)",backgroundSize:"contain"}}></div>
+                    <p> Successfully organized Summer Startup Festival , an Entrepreneurship summit multiple times in College.</p>
             </div>
         </div>
         
