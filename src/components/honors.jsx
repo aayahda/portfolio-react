b9c6dee0e887d87eb56b1b1cd9d4b59e50e1ba03
aayahda <aadhyakraj@gmail.com>
2024-05-29 23:04:57 +0530

    key accomplishment added

--- src/components/honors.jsx
+++ src/components/honors.jsx
@@ -3,8 +3,29 @@ import '../honors.css';
 
 
 function Honors(){
-    return (<div className="honors">
-        <h1> Key Accomplishments</h1></div>);
+    return (
+    <div className="honors">
+        <h1> Key Accomplishments</h1>
+        <div className="honor-box">
+            <div className="box">
+                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/89/bb/ca/89bbcab73b8d5d37272c2ed534af1237.jpg)",backgroundSize:"contain"}}></div>
+                    <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
+            </div>
+            <div className="box">
+                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/89/bb/ca/89bbcab73b8d5d37272c2ed534af1237.jpg)",backgroundSize:"contain"}}></div>
+                    <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
+            </div>
+            <div className="box">
+                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/89/bb/ca/89bbcab73b8d5d37272c2ed534af1237.jpg)",backgroundSize:"contain"}}></div>
+                    <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
+            </div>
+            <div className="box">
+                <div className="bg-img" style={{backgroundImage:"url(https://i.pinimg.com/564x/89/bb/ca/89bbcab73b8d5d37272c2ed534af1237.jpg)",backgroundSize:"contain"}}></div>
+                    <p> Bagged a prize in National-level India Plastic Challenge Hackathon conducted by the Ministry of India.</p>
+            </div>
+        </div>
+        
+        </div>);
 }
 
 
